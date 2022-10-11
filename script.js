ae89b52495ab6759568643c6a4be674c26a018b3
Fahir Muminovic <muminovicfahir998@gmail.com>
2022-10-12 00:42:12 +0200

    added answer check and next question display

--- script.js
+++ script.js
@@ -1,13 +1,16 @@
 const question = document.getElementById('question');
-const answers = document.querySelectorAll('p.answerText');
+const answersText = document.querySelectorAll('p.answerText');
+const answerContainers = document.querySelectorAll('.answer-wrapper');
 const questionNumber = document.getElementById('questionNumber');
+const modal = document.getElementById('modal');
 
 const quizData = [];
-
-const test = ['asdsfda', 'asdsfda', 'asdsfda', 'asdsfda'];
+const correctAnswers = [];
+const incorrectAnswers = [];
 
 fetchQuestions();
 
+//functions
 async function fetchQuestions() {
 	const response = await fetch('https://the-trivia-api.com/api/questions?limit=20');
 	const data = await response.json();
@@ -21,12 +24,15 @@ async function fetchQuestions() {
 			incorrectAnswers: `${el.incorrectAnswers}`,
 		};
 
-		addData(fetchedData);
+		addData(quizData, fetchedData);
+		addData(correctAnswers, el.correctAnswer);
+		addData(incorrectAnswers, el.incorrectAnswer);
 	});
 
 	updateDOM(quizData);
 }
 
+//pupulates the UI with the question and answers
 function updateDOM(providedData = quizData) {
 	const questionNum = questionNumber.value - 1;
 
@@ -37,7 +43,7 @@ function updateDOM(providedData = quizData) {
 			let counter = 0;
 			const incorrectAnswersArr = element.incorrectAnswers.split(',');
 
-			answers.forEach((answer, index) => {
+			answersText.forEach((answer, index) => {
 				if (index === correctAnwserPosition) {
 					answer.textContent = element.correctAnswer;
 				} else {
@@ -47,13 +53,39 @@ function updateDOM(providedData = quizData) {
 			});
 		}
 	});
+
+	questionNumber.nextElementSibling.textContent = `${questionNumber.value}/${questionNumber.max}`;
 }
 
-/* Helper functions */
-const addData = (obj) => {
-	quizData.push(obj);
+/* helper functions */
+const addData = (arr, el) => {
+	arr.push(el);
 };
 
 const randomizeCorrectAnswer = () => {
 	return Math.floor(Math.random() * 4);
 };
+
+//event listeners
+//check answer
+answerContainers.forEach((answerContainer) => {
+	answerContainer.addEventListener('click', (event) => {
+		if (correctAnswers.includes(answerContainer.lastElementChild.textContent)) {
+			answerContainer.classList.add('correct');
+			questionNumber.value += 1;
+
+			setTimeout(() => {
+				answerContainer.classList.remove('correct');
+				updateDOM();
+			}, 3000);
+		} else {
+			answerContainer.classList.add('incorrect');
+			questionNumber.value += 1;
+
+			setTimeout(() => {
+				answerContainer.classList.remove('incorrect');
+				updateDOM();
+			}, 3000);
+		}
+	});
+});
